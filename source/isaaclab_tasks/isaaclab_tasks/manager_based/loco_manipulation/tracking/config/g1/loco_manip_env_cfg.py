7e13e4df73782135ce4dec55b5ca69c840bfeaf7
eric <erickun0125@snu.ac.kr>
2025-08-14 23:14:06 +0900

    [init] update base loco-mani Env for g1

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/loco_manipulation/tracking/config/g1/loco_manip_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/loco_manipulation/tracking/config/g1/loco_manip_env_cfg.py
@@ -2,23 +2,31 @@
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
-
+import copy
 import math
 
-from .robots.unitree import G129_CFG_WITH_DEX3_BASE_FLOATING
-
-from isaaclab.managers import EventTermCfg
-from isaaclab.managers import ObservationGroupCfg as ObsGroup
-from isaaclab.managers import ObservationTermCfg as ObsTerm
-from isaaclab.managers import RewardTermCfg as RewTerm
-from isaaclab.managers import SceneEntityCfg
-from isaaclab.managers import TerminationTermCfg as TermTerm
+import isaaclab.sim as sim_utils
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.envs import ManagerBasedRLEnvCfg
+from isaaclab.managers import (
+    EventTermCfg as EventTerm,
+    ObservationGroupCfg as ObsGroup,
+    ObservationTermCfg as ObsTerm,
+    RewardTermCfg as RewTerm,
+    SceneEntityCfg,
+    TerminationTermCfg as TermTerm,
+)
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import ContactSensorCfg
+from isaaclab.terrains import TerrainImporterCfg
 from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR, ISAACLAB_NUCLEUS_DIR
 from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 
 import isaaclab_tasks.manager_based.locomotion.velocity.mdp as mdp
 import isaaclab_tasks.manager_based.manipulation.reach.mdp as manipulation_mdp
-from isaaclab_tasks.manager_based.locomotion.velocity.velocity_env_cfg import LocomotionVelocityRoughEnvCfg, EventCfg
+
+from .robots.unitree import G129_CFG_WITH_DEX3_BASE_FLOATING
 
 # G129 specific joint names (29 DOF with DEX3 hands)
 LEG_JOINT_NAMES = [
@@ -62,7 +70,46 @@ CONTROLLED_JOINTS = LEG_JOINT_NAMES + WAIST_JOINT_NAMES + ARM_JOINT_NAMES
 
 
 @configclass
-class G1LocoManipRewards:
+class G1LocoManipSceneCfg(InteractiveSceneCfg):
+    """Configuration for the terrain scene with a legged robot."""
+
+    # Ground terrain
+    terrain = TerrainImporterCfg(
+        prim_path="/World/ground",
+        terrain_type="plane",
+        terrain_generator=None,
+        collision_group=-1,
+        physics_material=sim_utils.RigidBodyMaterialCfg(
+            friction_combine_mode="multiply",
+            restitution_combine_mode="multiply",
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+        visual_material=sim_utils.MdlFileCfg(
+            mdl_path=f"{ISAACLAB_NUCLEUS_DIR}/Materials/TilesMarbleSpiderWhiteBrickBondHoned/TilesMarbleSpiderWhiteBrickBondHoned.mdl",
+            project_uvw=True,
+            texture_scale=(0.25, 0.25),
+        ),
+        debug_vis=False,
+    )
+    # Robots
+    robot: ArticulationCfg = copy.deepcopy(G129_CFG_WITH_DEX3_BASE_FLOATING)
+    robot.prim_path = "{ENV_REGEX_NS}/Robot"
+    # Sensors
+    contact_forces = ContactSensorCfg(prim_path="{ENV_REGEX_NS}/Robot/.*", history_length=3, track_air_time=True)
+    # Lights
+    sky_light = AssetBaseCfg(
+        prim_path="/World/skyLight",
+        spawn=sim_utils.DomeLightCfg(
+            intensity=750.0,
+            texture_file=f"{ISAAC_NUCLEUS_DIR}/Materials/Textures/Skies/PolyHaven/kloofendal_43d_clear_puresky_4k.hdr",
+        ),
+    )
+
+
+@configclass
+class G1LocoManipRewardsCfg:
+    """Reward terms for the G1 loco-manipulation task."""
     joint_vel_hip_yaw = RewTerm(
         func=mdp.joint_vel_l2,
         weight=-0.001,
@@ -219,16 +266,25 @@ class G1LocoManipRewards:
         weight=-100.0,
     )
 
+@configclass
+class G1LocoManipActionsCfg:
+    """Action specifications for the MDP."""
+
+    joint_pos = mdp.JointPositionActionCfg(
+        asset_name="robot",
+        joint_names=CONTROLLED_JOINTS,
+        scale=0.5,
+        use_default_offset=True,
+    )
 
 @configclass
-class G1LocoManipObservations:
+class G1LocoManipObservationsCfg:
+    """Observation terms grouped for policy and critic networks."""
 
     @configclass
     class PolicyCfg(ObsGroup):
-        base_lin_vel = ObsTerm(
-            func=mdp.base_lin_vel,
-            noise=Unoise(n_min=-0.1, n_max=0.1),
-        )
+        """Policy observations used as the actor input."""
+
         base_ang_vel = ObsTerm(
             func=mdp.base_ang_vel,
             noise=Unoise(n_min=-0.2, n_max=0.2),
@@ -253,23 +309,72 @@ class G1LocoManipObservations:
             func=mdp.joint_pos_rel,
             params={"asset_cfg": SceneEntityCfg("robot", joint_names=CONTROLLED_JOINTS)},
             noise=Unoise(n_min=-0.01, n_max=0.01),
+            history_length=3
         )
         joint_vel = ObsTerm(
             func=mdp.joint_vel_rel,
             params={"asset_cfg": SceneEntityCfg("robot", joint_names=CONTROLLED_JOINTS)},
             noise=Unoise(n_min=-1.5, n_max=1.5),
+            history_length=3
         )
-        actions = ObsTerm(func=mdp.last_action)
+        actions = ObsTerm(func=mdp.last_action, history_length=3)
 
-        def __post_init__(self):
+        def __post_init__(self) -> None:
             self.enable_corruption = True
             self.concatenate_terms = True
 
-    policy = PolicyCfg()
+    @configclass
+    class CriticCfg(ObsGroup):
+        """Critic observations - includes privileged information."""
 
+        # Privileged base state information
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel)
+        base_pos_z = ObsTerm(func=mdp.base_pos_z)
+
+        base_ang_vel = ObsTerm(
+            func=mdp.base_ang_vel,
+        )
+        projected_gravity = ObsTerm(
+            func=mdp.projected_gravity,
+        )
+        velocity_commands = ObsTerm(
+            func=mdp.generated_commands,
+            params={"command_name": "base_velocity"},
+        )
+        left_ee_pose_command = ObsTerm(
+            func=mdp.generated_commands,
+            params={"command_name": "left_ee_pose"},
+        )
+        right_ee_pose_command = ObsTerm(
+            func=mdp.generated_commands,
+            params={"command_name": "right_ee_pose"},
+        )
+        # Joint states with history
+        joint_pos = ObsTerm(
+            func=mdp.joint_pos_rel,
+            params={"asset_cfg": SceneEntityCfg("robot", joint_names=CONTROLLED_JOINTS)},
+            history_length=3
+        )
+        joint_vel = ObsTerm(
+            func=mdp.joint_vel_rel,
+            params={"asset_cfg": SceneEntityCfg("robot", joint_names=CONTROLLED_JOINTS)},
+            history_length=3
+        )
+        
+        # Action history
+        actions = ObsTerm(func=mdp.last_action, history_length=3)
+
+        def __post_init__(self) -> None:
+            self.enable_corruption = False  # Critic gets clean observations
+            self.concatenate_terms = True
+
+    # Observation groups
+    policy: PolicyCfg = PolicyCfg()
+    critic: CriticCfg = CriticCfg()
 
 @configclass
-class G1LocoManipCommands:
+class G1LocoManipCommandsCfg:
+    """Command generators for base velocity and end-effector poses."""
     base_velocity = mdp.UniformVelocityCommandCfg(
         asset_name="robot",
         resampling_time_range=(10.0, 10.0),
@@ -317,44 +422,122 @@ class G1LocoManipCommands:
 
 
 @configclass
-class G1Events(EventCfg):
+class G1LocoManipEventsCfg:
+    """Disturbance and domain-randomization events."""
+
+    reset_base = EventTerm(
+        func=mdp.reset_root_state_uniform,
+        mode="reset",
+        params={
+            "pose_range": {"x": (-0.5, 0.5), "y": (-0.5, 0.5), "yaw": (-3.14, 3.14)},
+            "velocity_range": {
+                "x": (0.0, 0.0),
+                "y": (0.0, 0.0),
+                "z": (0.0, 0.0),
+                "roll": (0.0, 0.0),
+                "pitch": (0.0, 0.0),
+                "yaw": (0.0, 0.0),
+            },
+        },
+    )
+
+    reset_robot_joints = EventTerm(
+        func=mdp.reset_joints_by_scale,
+        mode="reset",
+        params={
+            "position_range": (0.5, 1.5),
+            "velocity_range": (0.0, 0.0),
+        },
+    )
+
     # Add an external force to simulate a payload being carried.
-    left_hand_force = EventTermCfg(
+    left_hand_force = EventTerm(
         func=mdp.apply_external_force_torque,
         mode="interval",
         interval_range_s=(10.0, 15.0),
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="left_wrist_yaw_link"),
-            "force_range": (-10.0, 10.0),
+            "force_range": (-1.0, 1.0),
             "torque_range": (-1.0, 1.0),
         },
     )
 
-    right_hand_force = EventTermCfg(
+    right_hand_force = EventTerm(
         func=mdp.apply_external_force_torque,
         mode="interval",
         interval_range_s=(10.0, 15.0),
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names="right_wrist_yaw_link"),
-            "force_range": (-10.0, 10.0),
+            "force_range": (-1.0, 1.0),
             "torque_range": (-1.0, 1.0),
         },
     )
+    
+    # push the base to simulate a payload being carried.
+    push_base = EventTerm(
+        func=mdp.push_by_setting_velocity,
+        mode="interval",
+        interval_range_s=(3.0, 17.0),
+        params={
+            "velocity_range": {
+                "x": (-0.1, 0.1),
+                "y": (-0.1, 0.1),
+                "z": (-0.1, 0.1),
+                "roll": (-0.1, 0.1),
+                "pitch": (-0.1, 0.1),
+                "yaw": (-0.1, 0.1),
+            },
+        },
+    )
+    
+    # randomize the physics material of the robot.
+    physics_material = EventTerm(
+        func=mdp.randomize_rigid_body_material,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "static_friction_range": (0.8, 1.2),
+            "dynamic_friction_range": (0.6, 1.0),
+            "restitution_range": (0.0, 0.0),
+            "num_buckets": 64,
+        },
+    )
 
+    # add mass to the base to simulate a payload being carried.
+    add_base_mass = EventTerm(
+        func=mdp.randomize_rigid_body_mass,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="pelvis"),
+            "mass_distribution_params": (-5.0, 5.0),
+            "operation": "add",
+        },
+    )
 
-@configclass
-class G1ActionsCfg:
-    """Action specifications for the MDP."""
+    # randomize the center of mass of the base to simulate a payload being carried.
+    base_com = EventTerm(
+        func=mdp.randomize_rigid_body_com,
+        mode="startup",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="pelvis"),
+            "com_range": {"x": (-0.05, 0.05), "y": (-0.05, 0.05), "z": (-0.01, 0.01)},
+        },
+    )
 
-    joint_pos = mdp.JointPositionActionCfg(
-        asset_name="robot",
-        joint_names=LEG_JOINT_NAMES + WAIST_JOINT_NAMES + ARM_JOINT_NAMES,
-        scale=0.5,
-        use_default_offset=True,
+    # randomize the joint parameters of the robot.
+    actuator_gains = EventTerm(
+        func=mdp.randomize_actuator_gains,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", joint_names=CONTROLLED_JOINTS),
+            "stiffness_distribution_params": (0.95, 1.05),
+            "damping_distribution_params": (0.95, 1.05),
+            "operation": "scale",
+        },
     )
 
 @configclass
-class G1TerminationsCfg:
+class G1LocoManipTerminationsCfg:
     """Termination terms for the MDP."""
 
     time_out = TermTerm(func=mdp.time_out, time_out=True)
@@ -369,52 +552,49 @@ class G1TerminationsCfg:
         func=mdp.bad_orientation,
         params={"limit_angle": 2.0},
     )
+    base_height = TermTerm(
+        func=mdp.root_height_below_minimum,
+        params={"minimum_height": 0.3},
+    )
+
+@configclass
+class G1LocoManipCurriculumCfg:
+    """Curriculum terms for the MDP."""
+
+    #terrain_levels = CurrTerm(func=mdp.terrain_levels_vel)
 
 
 @configclass
-class G1LocoManipEnvCfg(LocomotionVelocityRoughEnvCfg):
-    rewards: G1LocoManipRewards = G1LocoManipRewards()
-    observations: G1LocoManipObservations = G1LocoManipObservations()
-    commands: G1LocoManipCommands = G1LocoManipCommands()
-    actions: G1ActionsCfg = G1ActionsCfg()
-    terminations: G1TerminationsCfg = G1TerminationsCfg()
+class G1LocoManipEnvCfg(ManagerBasedRLEnvCfg):
+    """Environment configuration for G1 loco-manipulation."""
+    # Scene
+    scene: G1LocoManipSceneCfg = G1LocoManipSceneCfg(num_envs=4096, env_spacing=2.5)
+    # r,a,o,c
+    rewards: G1LocoManipRewardsCfg = G1LocoManipRewardsCfg()
+    actions: G1LocoManipActionsCfg = G1LocoManipActionsCfg()
+    observations: G1LocoManipObservationsCfg = G1LocoManipObservationsCfg()
+    commands: G1LocoManipCommandsCfg = G1LocoManipCommandsCfg()
+    # Extra settings
+    terminations: G1LocoManipTerminationsCfg = G1LocoManipTerminationsCfg()
+    events: G1LocoManipEventsCfg = G1LocoManipEventsCfg()
+    curriculum: G1LocoManipCurriculumCfg = G1LocoManipCurriculumCfg()
 
-    def __post_init__(self):
-        super().__post_init__()
+    def __post_init__(self) -> None:
+        # General settings
+        self.decimation = 4
+        self.episode_length_s = 20.0
+        # Simulation settings
+        self.sim.dt = 0.005
+        self.sim.render_interval = self.decimation
+        self.sim.physics_material = self.scene.terrain.physics_material
+        self.sim.physx.gpu_max_rigid_patch_count = 10 * 2**15
 
-        self.episode_length_s = 14.0
-
-        # Replace robot with G129 (with DEX3 hands and floating base)
-        import copy
-        g1_robot_cfg = copy.deepcopy(G129_CFG_WITH_DEX3_BASE_FLOATING)
-        g1_robot_cfg.prim_path = "{ENV_REGEX_NS}/Robot"
-        self.scene.robot = g1_robot_cfg
-
-        # Update events for G1 body names
-        if hasattr(self.events, 'add_base_mass') and self.events.add_base_mass is not None:
-            self.events.add_base_mass.params["asset_cfg"] = SceneEntityCfg("robot", body_names="pelvis")
-        if hasattr(self.events, 'base_external_force_torque') and self.events.base_external_force_torque is not None:
-            self.events.base_external_force_torque.params["asset_cfg"] = SceneEntityCfg("robot", body_names="pelvis")
-        if hasattr(self.events, 'base_com') and self.events.base_com is not None:
-            self.events.base_com.params["asset_cfg"] = SceneEntityCfg("robot", body_names="pelvis")
-        
-        # Rewards:
-        self.rewards.flat_orientation_l2.weight = -10.5
-        self.rewards.termination_penalty.weight = -100.0
-
-        # Change terrain to flat.
-        self.scene.terrain.terrain_type = "plane"
-        self.scene.terrain.terrain_generator = None
-        # Remove height scanner and terrain curriculum if they exist
-        if hasattr(self.scene, 'height_scanner'):
-            self.scene.height_scanner = None
-        if hasattr(self.observations.policy, 'height_scan'):
-            self.observations.policy.height_scan = None
-        if hasattr(self.curriculum, 'terrain_levels'):
+        # Flat terrain has no generator; disable terrain curriculum if present
+        if hasattr(self.curriculum, "terrain_levels"):
             self.curriculum.terrain_levels = None
 
-
 class G1LocoManipEnvCfg_PLAY(G1LocoManipEnvCfg):
+    """Smaller play configuration for interactive testing."""
 
     def __post_init__(self) -> None:
         super().__post_init__()
@@ -425,7 +605,9 @@ class G1LocoManipEnvCfg_PLAY(G1LocoManipEnvCfg):
         # Disable randomization for play.
         self.observations.policy.enable_corruption = False
         # Remove random pushing if it exists
-        if hasattr(self.events, 'base_external_force_torque'):
-            self.events.base_external_force_torque = None
-        if hasattr(self.events, 'push_robot'):
-            self.events.push_robot = None
+        if hasattr(self.events, "left_hand_force"):
+            self.events.left_hand_force = None
+        if hasattr(self.events, "right_hand_force"):
+            self.events.right_hand_force = None
+        if hasattr(self.events, "push_base"):
+            self.events.push_base = None
